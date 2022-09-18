3fe09d4b8f5673af8440b80e93427f16eb8742d7
MaryHanson11 <maryhanson@ufl.edu>
2022-09-18 15:25:38 -0400

    Added part 2 project code for Flix assignment

---
+++ Flix/MovieGridCellCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  Flix
+//
+//  Created by Mary Hanson on 9/18/22.
+//
+
+import UIKit
+
+class MovieGridCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -20,7 +20,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         tableView.dataSource = self
         tableView.delegate = self
         
-        print("Hello")
         
         let url = URL(string: "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
@@ -35,8 +34,6 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
                 self.movies = dataDictionary["results"] as! [[String:Any]]
                  
                 self.tableView.reloadData()
-                 
-                print(dataDictionary)
 
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -70,4 +67,28 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         
         return cell
     }
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    //for sending data
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        print("loading the details")
+        
+        //find selected movie
+        let cell = sender as! UITableViewCell // sender tracks which cell calls
+        let indexPath = tableView.indexPath(for: cell)! // tableview has func for finding cell ind
+        let movie = movies[indexPath.row]
+        
+        //pass selected movie to details
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie // passes found movie to var in detailsViewController
+        
+        //get rid of highlighted selection
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
 }
